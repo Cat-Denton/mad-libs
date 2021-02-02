3f62e8660ed8bf7df161c0d9bba4464e8cb1c518
Cat-Denton <willwdenton@gmail.com>
2021-02-01 21:23:18 -0800

    Simplified scripts.js to work off of one array of strings and a forEach loop on that array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
-    const inputs = [$("input#person1").val(), $("input#person2").val(), $("input#animal").val(), $("input#exclamation").val(), $("input#verb").val(), $("input#noun").val()]
-
-    const classes = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"]
-
-    let iterator = 0;
-
-    inputs.forEach(function(element) {
-      $(classes[iterator]).text(element);
-      iterator += 1;
+    const classes = ["person1", "person2", "animal", "exclamation", "verb", "noun"]
+    
+    classes.forEach(function(element) {
+      let input = $("input#" + element).val()
+      $("." + element).text(input);
     });
     
     $("#story").show();
